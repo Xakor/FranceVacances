bf34f5a15ffe41652e4347ca661350e46318f497
Xakor <fha.post@gmail.com>
2015-11-24 11:09:38 +0100

    Finished basic layout for login page, started working on main page so that we can integrate the login page

---
+++ France Vacances/France Vacances.Windows/ViewModel/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using France_Vacances.Annotations;
+
+namespace France_Vacances.ViewModel
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        // Login Page ViewModel Code
+
+
+
+        // INotifyPropertyChanged Interface Integration
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
