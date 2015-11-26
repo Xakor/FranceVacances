af56182947dd20ab8001ddef0796e9a7fb41b0ea
Xakor <fha.post@gmail.com>
2015-11-26 10:22:33 +0100

    Reworked the cityModel class. Reworked the main page to utilize a list of all cities created with the constructor

--- France Vacances/France Vacances.Windows/Model/CityModel.cs
+++ France Vacances/France Vacances.Windows/Model/CityModel.cs
@@ -6,31 +6,62 @@ using System.Threading.Tasks;
 
 namespace France_Vacances.Model
 {
-    class CityModel
+    public class CityModel
     {
-        private string _name;
-        private double _minPrice;
+        // Instance Field
+        private string _cityName;
+        private double _basePrice;
+        private string _tagline;
+        private string _imgPath;
 
-        public string Name
+        // Set up properties
+        public string CityName
         {
-            get { return _name; }
-            set { _name = value; }
+            get { return _cityName; }
+            set { _cityName = value; }
         }
-        public double MinPrice
+        public double BasePrice
         {
-            get { return _minPrice; }
-            set { _minPrice = value; }
+            get { return _basePrice; }
+            set { _basePrice = value; }
         }
+        public string Tagline
+        {
+            get { return _tagline; }
+            set { _tagline = value; }
+        }
+        public string ImgPath
+        {
+            get { return _imgPath; }
+            set { _imgPath = value; }
+        }
+        
 
         public override string ToString()
         {
-            return Name;
+            return CityName.ToString();
         }
 
-        public CityModel(string name, double minPrice)
+        //Constructor
+        public CityModel(string name, double minPrice, string tagline, string imgPath)
         {
-            this._name = name;
-            this._minPrice = minPrice;
+            this._cityName = name;
+            this._basePrice = minPrice;
+            this._tagline = tagline;
+            this._imgPath = imgPath;
+        }
+
+        public static CityModel[] CityModels()
+        {
+            return new[]
+            {
+                new CityModel("Paris", 400, "The Capitol of France", "../assets/france_faded.png"),
+                new CityModel("Nice", 450, "something, something", "../assets/france_faded.png"),
+                new CityModel("Montpillier", 350, "Ubisoft have their headquarter there", "../assets/france_faded.png"),
+                new CityModel("Le Mans", 370, "Wroom wroom, boom boom", "../assets/france_faded.png"),
+                new CityModel("Carcassonne", 290, "Castles and board games", "../assets/france_faded.png"),
+                new CityModel("Versailles", 450, "You know you like it", "../assets/france_faded.png"),
+            };
         }
     }
 }
---
+++ France Vacances/France Vacances.Windows/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using France_Vacances.Annotations;
+using France_Vacances.Model;
+
+namespace France_Vacances.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<CityModel> _citiesCollection;
+        private CityModel _selectedCity;
+
+        public ObservableCollection<CityModel> CitiesCollection
+        {
+            get { return _citiesCollection; }
+            set { _citiesCollection = value; }
+        }
+
+        public CityModel SelectedCity
+        {
+            get { return _selectedCity; }
+            set
+            {
+                _selectedCity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            CitiesCollection = new ObservableCollection<CityModel>(CityModel.CityModels());
+        }
+
+        // inotify interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
