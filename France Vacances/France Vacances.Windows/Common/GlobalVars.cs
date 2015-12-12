291d308a623b330308e41c480de9d6479a04c39c
Frank Heldorf Andersen <fran1475@edu.easj.dk>
2015-12-12 13:23:26 +0100

    Added GlobalVars Document, with specific global variables for logged in users & selected apartments/cities

---
+++ France Vacances/France Vacances.Windows/Common/GlobalVars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace France_Vacances.Common
+{
+    public class GlobalVars
+    {
+
+        //Vars related to user logins
+        public bool Global_LoggedInBool = false;
+        public string Global_LoggedInName;
+
+        //Vars related to selected elemtns
+        public int Global_SelectedCity;
+        public int Global_selectedApartment;
+    }
+}
